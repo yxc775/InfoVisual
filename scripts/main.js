226d702693dc27d96a6ab5986ca1480b798174cb
yxc775 <yxc775@case.edu>
2019-09-28 17:52:16 -0400

    four images layout almost finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,16 @@
-
-var tick = 0
-var stateIncrement = 2
-
-var stop = function(){
-	stateIncrement = 0
-}
-
-var start = function(){
-	stateIncrement = 2
-}
-
-var reset = function(){
-	tick = 0
-}
-
-
 ;(function() {
 
 	var margin = { top: 10, right: 10, bottom: 100, left: 50 };
-	var margin = { top: 10, right: 10, bottom: 100, left: 50 };
 	var width = 600;
 	var height = 300;
-	var duration = 250;
-	var globalx = 0;
-	var size = 60
-	var t;
 
-	var dataXRange = { min: 0, max: 150 };
-	var dataYRange = { min: 0, max: 150 };
-	var xAxisLabelHeader = "X Header";
-	var yAxisLabelHeader = "Y Header";
 	var circleRadius = 4;
-
 	var data;
-	var chart;
-	var chartWidth;
-	var chartHeight;
-
 	init();
 
 	function init() {
 
 		console.log("initiating!")
-
-		chartWidth = width - margin.left - margin.right;
-		chartHeight = height - margin.top - margin.bottom;
 
 		// load data from json
 
@@ -52,32 +18,93 @@ var reset = function(){
 
 			data = json;
 			console.log("JSON loaded");
-			initializeChart();
+			div1 ="#KvGandKvT";
+			div2 = "#GvTandKGvT";
 
-
+			initializeChart(div1,"KvG",margin);
+			initializeChart(div1,"KGvT",margin);
+			initializeChart(div2,"KvT",margin);
+			initializeChart(div2,"GvT",margin);
 		//load data from xlsx
-
-
 		})
 
 	}//end init
 
-	function initializeChart() {
-		chart = d3.select("#chartDiv").append("svg")
+	function initializeChart(selector,mode,margin) {
+		var xAxisLabelHeader = "X Header";
+		var yAxisLabelHeader = "Y Header";
+		if(mode == "KvG"){
+			xAxisLabelHeader = "Blood glucose mg per dL"
+			yAxisLabelHeader = "Blood ketones mg per dL"
+		}
+		else if(mode == "KvT"){
+			xAxisLabelHeader = "Days on PKT"
+			yAxisLabelHeader = "Blood ketones mg per dL"
+		}
+		else if(mode == "GvT"){
+			xAxisLabelHeader = "Days on PKT"
+			yAxisLabelHeader = "Blood glucose mg per dL"
+		}
+		else if(mode == "KGvsT"){
+			xAxisLabelHeader = "Days on PKT"
+			yAxisLabelHeader = "Glucose Ketone Index"
+		}
+		else{
+			console.log("Invalid mode selected");
+		}
+
+		var chart;
+		var chartWidth = width - margin.left - margin.right;
+		var chartHeight = height - margin.top - margin.bottom;
+		chart = d3.select(selector).append("svg")
 			.attr("width", width)
 			.attr("height", height);
-
 		chart.plotArea = chart.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-			createAxes();
-			drawDots();
+
+			createAxes(chart,mode,chartWidth,chartHeight,xAxisLabelHeader,yAxisLabelHeader);
+			drawDots(chart,mode,chartWidth,chartHeight);
 	}
 
 
 
-	function createAxes() {
+	function createAxes(chart,mode,chartWidth,chartHeight,xAxisLabelHeader,yAxisLabelHeader) {
+		var dataXRange;
+		var dataYRange;
+		var minx;
+		var miny;
+		var maxx;
+		var maxy;
+		if(mode == "KvG"){
+			minx = d3.min(data, function(d) { return +d.Blood_glucose_mg_per_dL});
+			miny = d3.min(data, function(d) { return +d.Blood_ketones_mg_per_dL});
+			maxx = d3.max(data, function(d) { return +d.Blood_glucose_mg_per_dL});
+			maxy = d3.max(data, function(d) { return +d.Blood_ketones_mg_per_dL});
+		}
+		else if(mode == "KvT"){
+			minx = d3.min(data, function(d) { return +d.Days_on_PKT});
+			miny = d3.min(data, function(d) { return +d.Blood_ketones_mg_per_dL});
+			maxx = d3.max(data, function(d) { return +d.Days_on_PKT});
+			maxy = d3.max(data, function(d) { return +d.Blood_ketones_mg_per_dL});
+		}
+		else if(mode == "GvT"){
+			minx = d3.min(data, function(d) { return +d.Days_on_PKT});
+			miny = d3.min(data, function(d) { return +d.Blood_glucose_mg_per_dL});
+			maxx = d3.max(data, function(d) { return +d.Days_on_PKT});
+			maxy = d3.max(data, function(d) { return +d.Blood_glucose_mg_per_dL});
+		}
+		else if(mode == "KGvsT"){
+			xAxisLabelHeader = "Days on PKT"
+			yAxisLabelHeader = "Glucose Ketone Index"
+		}
+		else{
+			console.log("Invalid mode selected");
+		}
 
+		dataXRange = { min: minx, max: maxx };
+		dataYRange = { min: miny, max: maxy };
 		// x axis
+
 		chart.xScale = d3.scaleLinear()
 			.domain([dataXRange.min,dataXRange.max])
 			.range([0, chartWidth]);
@@ -117,14 +144,43 @@ var reset = function(){
 								.text(yAxisLabelHeader);
 	}
 
-	function drawDots() {
-		var dots = chart.plotArea.selectAll(".KvG").data(data)
+	function drawDots(chart,mode,chartWidth,chartHeight) {
+		var dots = chart.plotArea.selectAll(".dots").data(data)
 		dots.enter().append("circle").on("click", function(d) {
-		 							console.log("circle: ", d.Blood_glucose_mg_per_dL, ", ", d.Blood_ketones_mg_per_dL);
+								if(mode == "KvG"){
+									console.log("circle: ", d.Blood_glucose_mg_per_dL, ", ", d.Blood_ketones_mg_per_dL);
+								}
+								else if(mode == "KvT"){
+									console.log("circle: ", d.Days_on_PKT, ", ", d.Blood_ketones_mg_per_dL);
+								}
 		 						}).merge(dots)
  						.attr("class", "dot")
- 						.attr("cx", function(d) { return chart.xScale(d.Blood_glucose_mg_per_dL); })
- 						.attr("cy", function(d) { return chart.yScale(d.Blood_ketones_mg_per_dL); })
+ 						.attr("cx", function(d) {
+							if(mode == "KvG"){
+								return chart.xScale(d.Blood_glucose_mg_per_dL);
+						  }
+							else if(mode == "KvT" || mode == "GvT" || mode == "KGvT"){
+								return chart.xScale(d.Days_on_PKT);
+							}
+							else{
+								console.log("Error: unexpected datatype on x-axis?");
+							}
+						}
+						)
+ 						.attr("cy", function(d) {
+							if(mode == "KvG" || mode == "KvT"){
+								return chart.yScale(d.Blood_ketones_mg_per_dL);
+							}
+							else if(mode == "GvT"){
+								return chart.yScale(d.Blood_ketones_mg_per_dL);
+							}
+							else if(mode == "KGvT"){
+								return chart.yScale(d.Blood_ketones_mg_per_dL);
+							}
+							else{
+								console.log("Error: unexpeceted datatype on y-axis?")
+							}
+						 })
  						.attr("r",  circleRadius)
  						.attr("fill", "red")
 		// do something with the data here!
